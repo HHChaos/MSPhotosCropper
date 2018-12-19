5503e0b30f52f841b25301b0031846d3a07c6bd9
hhchaos <HHChaos@outlook.com>
2018-12-19 10:20:39 +0800

    添加项目文件。

---
+++ MSPhotosCropper/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace MSPhotosCropper
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            var file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var croppedImage = await OpenMSPhotosCropper(file);
+                if (croppedImage != null)
+                    Image.Source = croppedImage;
+            }
+        }
+
+        private async Task<BitmapImage> OpenMSPhotosCropper(StorageFile imageFile)
+        {
+            var inputFile = SharedStorageAccessManager.AddFile(imageFile);
+            var destination = await ApplicationData.Current.LocalFolder.CreateFileAsync("Cropped.jpg", CreationCollisionOption.ReplaceExisting);
+            var destinationFile = SharedStorageAccessManager.AddFile(destination);
+            var options = new LauncherOptions();
+            options.TargetApplicationPackageFamilyName = "Microsoft.Windows.Photos_8wekyb3d8bbwe";
+
+            var parameters = new ValueSet
+            {
+                { "InputToken", inputFile },
+                { "DestinationToken", destinationFile },
+                { "ShowCamera", false },
+                { "EllipticalCrop", true },                 //use circular cropper
+                { "CropWidthPixals", 300 },
+                { "CropHeightPixals", 300 }
+            };
+
+            var result = await Launcher.LaunchUriForResultsAsync(new Uri("microsoft.windows.photos.crop:"), options, parameters);
+
+            if (result.Status == LaunchUriStatus.Success && result.Result != null)
+            {
+                var stream = await destination.OpenReadAsync();
+                var bitmap = new BitmapImage();
+                await bitmap.SetSourceAsync(stream);
+                return bitmap;
+            }
+            return null;
+        }
+    }
+}
